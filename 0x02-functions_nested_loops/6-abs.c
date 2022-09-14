970ded7720b339c5c00879efed0b39ab70372b2f
lordskenzy <lordskenzy@gmail.com>
2022-09-14 02:39:24 -0700

    omo

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * returns an absolute value of an int
+ *
+ * @n: this file provides the values needed
+ *
+ * Return: 0;
+ */
+
+int _abs(int n)
+
+{
+
+	if (n < 0)
+
+	{
+
+		return (n * -1);
+
+	}
+
+	return (n);
+
+}
