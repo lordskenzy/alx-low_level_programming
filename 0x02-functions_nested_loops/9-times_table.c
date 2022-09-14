1e02856308ca19bb6d8f1356af75a68ab54d7ebd
lordskenzy <lordskenzy@gmail.com>
2022-09-14 12:55:09 -0700

    omo

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
- *
+ * times_table - prints the 9 times table, starting with 0
  * times_table - prints the 9 times table, starting with 0
  *
  */
 
 void times_table(void)
+
 {
 	int i, j, k;
 	for (i = 0; i < 10; i++)
